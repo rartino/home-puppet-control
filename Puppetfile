3097c30b0dac33c3e041928b68533ebe64bb3003
Rickard Armiento <gitcommits@armiento.net>
2021-04-05 19:22:12 +0200

    Change to plex

--- Puppetfile
+++ Puppetfile
@@ -6,13 +6,15 @@ mod 'puppetlabs/translate', '1.2.0'
 
 mod 'puppetlabs/ntp', '8.5.0'
 mod 'puppetlabs/firewall', '2.8.1'
+mod 'puppetlabs/apt', '7.7.1'
 
 mod 'locales',
     :git => 'https://github.com/saz/puppet-locales.git',
     :ref => 'beec43f6f079d8993087b105677d3847380ba5c3' #2.5.1
 
-mod 'samba',
-    :git => 'https://github.com/ajjahn/puppet-samba.git',
-    :ref => '7082cd5ca6de4cfbf18128dd33d1e33c6c04df5c' #0.5.0+
+# Replaced by own samba_fileserver module
+#mod 'samba',
+#    :git => 'https://github.com/ajjahn/puppet-samba.git',
+#    :ref => '7082cd5ca6de4cfbf18128dd33d1e33c6c04df5c' #0.5.0+
 
 # local module /etc/puppet/code/local-modules/public/hostname
--- site/roles/manifests/home_fileserver.pp
+++ site/roles/manifests/home_fileserver.pp
@@ -11,9 +11,35 @@ class roles::home_fileserver {
      use_ldap => true,
   }
 
-  class { 'minidlna':
-        dlna_listen_if => fact('networking.primary'),
-        dlna_media_dirs => [
+  #class { 'minidlna':
+  #      dlna_listen_if => fact('networking.primary'),
+  #      dlna_media_dirs => [
+  #          #"V,/disks/pingu/MEDIA/DVD-MOVIES/",
+  #          #"V,/disks/pingu/MEDIA/DVD-SERIES/",
+  #          #"V,/disks/pinga/MEDIA/DVD-MOVIES/",
+  #          #"V,/disks/pinga/MEDIA/DVD-SERIES/",
+  #          "A,/disks/pinga/MEDIA/CD/"],
+  #}
+
+  class {
+      'plex':
+        plex_listen_if => fact('networking.primary'),
+        plex_movies_dirs => [
+	  "/disks/pingu/MEDIA/DVD-MOVIES/",
+          "/disks/pinga/MEDIA/DVD-MOVIES/",  
+	],
+        plex_video_dirs => [
+	  "/disks/pingu/MEDIA/DVD-VIDEO/",
+          "/disks/pinga/MEDIA/DVD-VIDEO/",  
+	],	
+        plex_tv_series_dirs => [
+	  "/disks/pingu/MEDIA/DVD-SERIES/",
+          "/disks/pinga/MEDIA/DVD-SERIES/",  
+	],
+	plex_audio_dirs => [
+	  "/disks/pingu/MEDIA/CD/",
+	  "/disks/pinga/MEDIA/CD/",
+	],
             #"V,/disks/pingu/MEDIA/DVD-MOVIES/",
             #"V,/disks/pingu/MEDIA/DVD-SERIES/",
             #"V,/disks/pinga/MEDIA/DVD-MOVIES/",

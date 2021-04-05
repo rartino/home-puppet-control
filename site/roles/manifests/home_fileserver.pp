a77e816bf750bc8a7b669c6b86f1f1ae4f6d4409
Rickard Armiento <gitcommits@armiento.net>
2021-04-05 11:06:11 +0200

    Divert samba config

--- site/roles/manifests/home_fileserver.pp
+++ site/roles/manifests/home_fileserver.pp
@@ -7,41 +7,8 @@ class roles::home_fileserver {
     servers => [ 'pool.ntp.org' ],
   }
 
-  class {'samba::server':
-        workgroup               => 'WORKGROUP',
-        server_string           => "${::hostname}",
-        dns_proxy               => 'no',
-        usershare_allow_guests  => 'yes',
-        bind_interfaces_only    => 'no',
-        security                => 'user',
-  }
-
-  samba::server::share {'disks':
-    comment                   => 'Disks',
-    path                      => '/disks',
-    guest_only                => false,
-    guest_ok                  => true,
-    guest_account             => "guest",
-    browsable                 => true,
-    create_mask               => 0777,
-    force_create_mask         => 0777,
-    directory_mask            => 0777,
-    force_directory_mode      => 0777,
-    hosts_allow               => '127.0.0.1, 192.168.1.',
-    writable                  => false,
-    read_list                 => "@users"
-  }
-
-  firewall { '100 allow samba access':
-    dport   => [139, 445],
-    proto  => 'tcp',
-    action => 'accept',
-  }
-
-  firewall { '101 allow samba udp access':
-    dport   => [137, 138],
-    proto  => 'udp',
-    action => 'accept',
+  class { 'samba_fileserver':
+     use_ldap=True,
   }
 
   class { 'minidlna':

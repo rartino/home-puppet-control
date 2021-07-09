80fb3e13ae3f7cce348abf9ee5738e6a0771777f
Rickard Armiento <gitcommits@armiento.net>
2021-07-10 01:29:57 +0200

    Fix reference to home_fileserver class

--- site/systems/manifests/minimoose.pp
+++ site/systems/manifests/minimoose.pp
@@ -6,7 +6,7 @@ class systems::minimoose {
         domain => "home",
       }
 
-      class { 'home_fileserver':
+      class { 'roles::home_fileserver':
 	main_net_if => "eno1",
 	bridge_net_if => "enp1s0",
       }
